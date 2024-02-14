2acd9ad3eaf18c4856bed6f935c0fab6affd6e77
Dave Faliskie <davefaliskie@gmail.com>
2024-02-14 01:55:07 +0100

    set full sceeen portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:go_green/game_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
+
   runApp(const GameApp());
 }
