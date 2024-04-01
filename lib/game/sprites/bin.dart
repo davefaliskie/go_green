39ea567e958c762926283e98089477f54a4c1e4b
Dave Faliskie <davefaliskie@gmail.com>
2024-04-01 21:30:19 +0100

    remove unused bin

--- lib/game/sprites/bin.dart
+++ lib/game/sprites/bin.dart
@@ -1,34 +1,7 @@
-import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/foundation.dart';
-import 'package:go_green/game/go_green_game.dart';
 import 'package:go_green/game/sprites/obstacle.dart';
 import 'package:go_green/game/sprites/player.dart';
-
-class Bin extends SpriteComponent
-    with HasGameRef<GoGreenGame>, CollisionCallbacks {
-  Bin({
-    required this.spritePath,
-  });
-
-  final String spritePath;
-
-  @override
-  Future<void> onLoad() async {
-    sprite = await Sprite.load(spritePath);
-    anchor = Anchor.center;
-    add(RectangleHitbox());
-  }
-
-  @override
-  bool onComponentTypeCheck(PositionComponent other) {
-    if (other is Obstacle || other is Bin) {
-      // do NOT collide with Other Obsticles or Bins
-      return false;
-    }
-    return super.onComponentTypeCheck(other);
-  }
-}
 
 class BinTrash extends Obstacle {
   BinTrash() : super(spritePath: 'bin_trash.png');
